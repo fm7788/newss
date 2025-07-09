8ea5352ae5053ab4b7e1d8d319e4c83aa450f9cf
fm7788 <128238659+fm7788@users.noreply.github.com>
2025-07-09 15:03:49 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,13 @@
 export function Footer() {
   return (
-    <>/**
-      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
-      <span>
-        <span>NewsNow © 2024 By </span>
-        <a href={Author.url} target="_blank">
-          {Author.name}
-        </a>
-      </span>*/
+    <>
+    //  <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
+    //  <span>
+  //    <span>NewsNow © 2024 By </span>
+   //    <a href={Author.url} target="_blank">
+   //       {Author.name}
+   //     </a>
+   //   </span>
     </>
   )
 }
