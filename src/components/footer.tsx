ea4a7b0f83e548de2770891158d81c5d5c307a31
fm7788 <128238659+fm7788@users.noreply.github.com>
2025-08-07 09:21:48 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,7 @@ export function Footer() {
   return (
     <>
       <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
-      <span>
         <span>TOPNews © 2025</span>
-        </a>
-      </span>
     </>
   )
 }
