db7e7787739bf22bffd7b78dbfb3f64c14c40e5e
fm7788 <128238659+fm7788@users.noreply.github.com>
2025-08-07 10:11:46 +0800

    Update server.ts

--- server/mcp/server.ts
+++ server/mcp/server.ts
@@ -7,7 +7,7 @@ import { description } from "./desc.js"
 export function getServer() {
   const server = new McpServer(
     {
-      name: "NewsNow",
+      name: "TOPNews",
       version: packageJSON.version,
     },
     { capabilities: { logging: {} } },
